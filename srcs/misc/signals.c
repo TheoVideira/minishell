e0f7967b81872314f43bcba741b1797dd3abcd72
mclaudel <mclaudel@student.42.fr>
2020-06-19 00:36:13 +0200

    fixed return code issue with signals

--- srcs/misc/signals.c
+++ srcs/misc/signals.c
@@ -6,7 +6,7 @@
 /*   By: user42 <user42@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/05/26 15:54:00 by user42            #+#    #+#             */
-/*   Updated: 2020/06/05 19:37:10 by user42           ###   ########.fr       */
+/*   Updated: 2020/06/19 00:29:12 by user42           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,6 @@ void	handle_sigint(int sig)
 {
 	int r;
 
-	g_mini.lastcall = 130;
 	if (!g_mini.childs)
 		r = write(1, "\n\e[1;35mOK-BOOMER\e[0m$>", 24);
 	else
@@ -24,6 +23,7 @@ void	handle_sigint(int sig)
 	if (r == -1)
 		fatal_error("write");
 	brutally_murder_childrens(sig);
+	g_mini.lastcall = 130;
 }
 
 void	handle_sigquit(int sig)
