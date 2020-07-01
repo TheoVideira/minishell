218d0eb49f69406f0c7971c225ac18a78c63f8c1
mclaudel <mclaudel@student.42.fr>
2020-07-01 19:51:04 +0200

    fixed a fd prob;em on redirection on single builtin redirection

--- srcs/interpreter/run_pipeline.c
+++ srcs/interpreter/run_pipeline.c
@@ -6,7 +6,7 @@
 /*   By: user42 <user42@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/06/02 05:08:55 by user42            #+#    #+#             */
-/*   Updated: 2020/07/01 16:43:26 by user42           ###   ########.fr       */
+/*   Updated: 2020/07/01 19:48:53 by user42           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -51,13 +51,44 @@ static int	is_single_builtin(t_cmd *c)
 	return (0);
 }
 
+static int	save_inout(int save[2])
+{
+	save[0] = dup(0);
+	if (save[0] == -1)
+		return (fatal_error("dup"));
+	save[1] = dup(1);
+	if (save[1] == -1)
+	{
+		close(save[0]);
+		return (fatal_error("dup"));
+	}
+	return (0);
+}
+
+static int	restore_inout(int save[2])
+{
+	int r1;
+	int r2;
+
+	r1 = dup2(save[0], 0);
+	r2 = dup2(save[1], 1);
+	if (r1 == -1 || r2 == -1)
+		return (fatal_error("dup2"));
+	close(save[0]);
+	close(save[1]);
+	return (0);
+}
+
 int			run_pipeline(t_pipeline *pi)
 {
 	t_list	*l;
 	int		len;
 	int		r;
+	int		save[2];
 
 	l = pi->cmds;
+	if ((r = save_inout(save)))
+		return (r);
 	len = ft_lstsize(pi->cmds);
 	if (len == 1 && (r = is_single_builtin((t_cmd *)l->content)))
 	{
@@ -68,6 +99,11 @@ int			run_pipeline(t_pipeline *pi)
 	else
 		r = run_processes(len, pi->cmds);
 	if (r)
+	{
+		restore_inout(save);
+		return (r);
+	}
+	if ((r = restore_inout(save)))
 		return (r);
 	return (g_mini.lastcall);
 }
